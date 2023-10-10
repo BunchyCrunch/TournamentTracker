d4ce45fef5e4f9e5f8d11282c6e5407b9a1de806
bunchycrunch <sparksj1127@gmail.com>
2023-10-10 17:05:18 -0600

    created Tournament dashboard

---
+++ TrackerUI/CreatePrizeForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TrackerUI
+{
+    partial class CreatePrizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            headerLabel = new Label();
+            placeNumberValue = new TextBox();
+            placeNumberLabel = new Label();
+            prizeAmountValue = new TextBox();
+            prizeAmountLabel = new Label();
+            placeNameValue = new TextBox();
+            placeNameLabel = new Label();
+            prizePercentageValue = new TextBox();
+            prizePercentageLabel = new Label();
+            orLabel = new Label();
+            createPrizeButton = new Button();
+            SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            headerLabel.AutoSize = true;
+            headerLabel.Font = new Font("Segoe UI Light", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            headerLabel.ForeColor = SystemColors.MenuHighlight;
+            headerLabel.Location = new Point(41, 47);
+            headerLabel.Margin = new Padding(5, 0, 5, 0);
+            headerLabel.Name = "headerLabel";
+            headerLabel.Size = new Size(209, 50);
+            headerLabel.TabIndex = 12;
+            headerLabel.Text = "Create Prize";
+            // 
+            // placeNumberValue
+            // 
+            placeNumberValue.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            placeNumberValue.Location = new Point(285, 147);
+            placeNumberValue.Margin = new Padding(5, 6, 5, 6);
+            placeNumberValue.Name = "placeNumberValue";
+            placeNumberValue.Size = new Size(237, 35);
+            placeNumberValue.TabIndex = 25;
+            // 
+            // placeNumberLabel
+            // 
+            placeNumberLabel.AutoSize = true;
+            placeNumberLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            placeNumberLabel.ForeColor = SystemColors.MenuHighlight;
+            placeNumberLabel.Location = new Point(56, 145);
+            placeNumberLabel.Margin = new Padding(5, 0, 5, 0);
+            placeNumberLabel.Name = "placeNumberLabel";
+            placeNumberLabel.Size = new Size(183, 37);
+            placeNumberLabel.TabIndex = 24;
+            placeNumberLabel.Text = "Place Number";
+            // 
+            // prizeAmountValue
+            // 
+            prizeAmountValue.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            prizeAmountValue.Location = new Point(285, 265);
+            prizeAmountValue.Margin = new Padding(5, 6, 5, 6);
+            prizeAmountValue.Name = "prizeAmountValue";
+            prizeAmountValue.Size = new Size(237, 35);
+            prizeAmountValue.TabIndex = 27;
+            // 
+            // prizeAmountLabel
+            // 
+            prizeAmountLabel.AutoSize = true;
+            prizeAmountLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            prizeAmountLabel.ForeColor = SystemColors.MenuHighlight;
+            prizeAmountLabel.Location = new Point(56, 263);
+            prizeAmountLabel.Margin = new Padding(5, 0, 5, 0);
+            prizeAmountLabel.Name = "prizeAmountLabel";
+            prizeAmountLabel.Size = new Size(176, 37);
+            prizeAmountLabel.TabIndex = 26;
+            prizeAmountLabel.Text = "Prize Amount";
+            // 
+            // placeNameValue
+            // 
+            placeNameValue.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            placeNameValue.Location = new Point(285, 206);
+            placeNameValue.Margin = new Padding(5, 6, 5, 6);
+            placeNameValue.Name = "placeNameValue";
+            placeNameValue.Size = new Size(237, 35);
+            placeNameValue.TabIndex = 29;
+            // 
+            // placeNameLabel
+            // 
+            placeNameLabel.AutoSize = true;
+            placeNameLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            placeNameLabel.ForeColor = SystemColors.MenuHighlight;
+            placeNameLabel.Location = new Point(56, 204);
+            placeNameLabel.Margin = new Padding(5, 0, 5, 0);
+            placeNameLabel.Name = "placeNameLabel";
+            placeNameLabel.Size = new Size(157, 37);
+            placeNameLabel.TabIndex = 28;
+            placeNameLabel.Text = "Place Name";
+            // 
+            // prizePercentageValue
+            // 
+            prizePercentageValue.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            prizePercentageValue.Location = new Point(285, 413);
+            prizePercentageValue.Margin = new Padding(5, 6, 5, 6);
+            prizePercentageValue.Name = "prizePercentageValue";
+            prizePercentageValue.Size = new Size(237, 35);
+            prizePercentageValue.TabIndex = 31;
+            // 
+            // prizePercentageLabel
+            // 
+            prizePercentageLabel.AutoSize = true;
+            prizePercentageLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            prizePercentageLabel.ForeColor = SystemColors.MenuHighlight;
+            prizePercentageLabel.Location = new Point(57, 409);
+            prizePercentageLabel.Margin = new Padding(5, 0, 5, 0);
+            prizePercentageLabel.Name = "prizePercentageLabel";
+            prizePercentageLabel.Size = new Size(212, 37);
+            prizePercentageLabel.TabIndex = 30;
+            prizePercentageLabel.Text = "Prize Percentage";
+            // 
+            // orLabel
+            // 
+            orLabel.AutoSize = true;
+            orLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            orLabel.ForeColor = SystemColors.MenuHighlight;
+            orLabel.Location = new Point(239, 334);
+            orLabel.Margin = new Padding(5, 0, 5, 0);
+            orLabel.Name = "orLabel";
+            orLabel.Size = new Size(85, 37);
+            orLabel.TabIndex = 32;
+            orLabel.Text = "- or - ";
+            orLabel.Click += orLabel_Click;
+            // 
+            // createPrizeButton
+            // 
+            createPrizeButton.FlatAppearance.BorderColor = Color.Silver;
+            createPrizeButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(102, 102, 102);
+            createPrizeButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(242, 242, 242);
+            createPrizeButton.FlatStyle = FlatStyle.Flat;
+            createPrizeButton.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            createPrizeButton.ForeColor = SystemColors.MenuHighlight;
+            createPrizeButton.Location = new Point(210, 535);
+            createPrizeButton.Name = "createPrizeButton";
+            createPrizeButton.Size = new Size(237, 88);
+            createPrizeButton.TabIndex = 33;
+            createPrizeButton.Text = "Create Prize";
+            createPrizeButton.UseVisualStyleBackColor = true;
+            // 
+            // CreatePrizeForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(567, 671);
+            Controls.Add(createPrizeButton);
+            Controls.Add(orLabel);
+            Controls.Add(prizePercentageValue);
+            Controls.Add(prizePercentageLabel);
+            Controls.Add(placeNameValue);
+            Controls.Add(placeNameLabel);
+            Controls.Add(prizeAmountValue);
+            Controls.Add(prizeAmountLabel);
+            Controls.Add(placeNumberValue);
+            Controls.Add(placeNumberLabel);
+            Controls.Add(headerLabel);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "CreatePrizeForm";
+            Text = "Create Prize";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label headerLabel;
+        private TextBox placeNumberValue;
+        private Label placeNumberLabel;
+        private TextBox prizeAmountValue;
+        private Label prizeAmountLabel;
+        private TextBox placeNameValue;
+        private Label placeNameLabel;
+        private TextBox prizePercentageValue;
+        private Label prizePercentageLabel;
+        private Label orLabel;
+        private Button createPrizeButton;
+    }
+}
---
+++ TrackerUI/TournamentDashboardForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TrackerUI
+{
+    partial class TournamentDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            headerLabel = new Label();
+            loadExistingTournamentDropDown = new ComboBox();
+            loadExistingTournamentLabel = new Label();
+            loadTournamentButton = new Button();
+            createTournamentButton = new Button();
+            SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            headerLabel.AutoSize = true;
+            headerLabel.Font = new Font("Segoe UI Light", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            headerLabel.ForeColor = SystemColors.MenuHighlight;
+            headerLabel.Location = new Point(116, 52);
+            headerLabel.Margin = new Padding(5, 0, 5, 0);
+            headerLabel.Name = "headerLabel";
+            headerLabel.Size = new Size(385, 50);
+            headerLabel.TabIndex = 13;
+            headerLabel.Text = "Tournament Dashboard";
+            // 
+            // loadExistingTournamentDropDown
+            // 
+            loadExistingTournamentDropDown.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            loadExistingTournamentDropDown.FormattingEnabled = true;
+            loadExistingTournamentDropDown.Location = new Point(82, 176);
+            loadExistingTournamentDropDown.Margin = new Padding(5, 6, 5, 6);
+            loadExistingTournamentDropDown.Name = "loadExistingTournamentDropDown";
+            loadExistingTournamentDropDown.Size = new Size(453, 38);
+            loadExistingTournamentDropDown.TabIndex = 20;
+            // 
+            // loadExistingTournamentLabel
+            // 
+            loadExistingTournamentLabel.AutoSize = true;
+            loadExistingTournamentLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            loadExistingTournamentLabel.ForeColor = SystemColors.MenuHighlight;
+            loadExistingTournamentLabel.Location = new Point(147, 133);
+            loadExistingTournamentLabel.Margin = new Padding(5, 0, 5, 0);
+            loadExistingTournamentLabel.Name = "loadExistingTournamentLabel";
+            loadExistingTournamentLabel.Size = new Size(322, 37);
+            loadExistingTournamentLabel.TabIndex = 19;
+            loadExistingTournamentLabel.Text = "Load Existing Tournament";
+            // 
+            // loadTournamentButton
+            // 
+            loadTournamentButton.FlatAppearance.BorderColor = Color.Silver;
+            loadTournamentButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(102, 102, 102);
+            loadTournamentButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(242, 242, 242);
+            loadTournamentButton.FlatStyle = FlatStyle.Flat;
+            loadTournamentButton.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            loadTournamentButton.ForeColor = SystemColors.MenuHighlight;
+            loadTournamentButton.Location = new Point(206, 257);
+            loadTournamentButton.Name = "loadTournamentButton";
+            loadTournamentButton.Size = new Size(204, 61);
+            loadTournamentButton.TabIndex = 21;
+            loadTournamentButton.Text = "Load Tournament";
+            loadTournamentButton.UseVisualStyleBackColor = true;
+            // 
+            // createTournamentButton
+            // 
+            createTournamentButton.FlatAppearance.BorderColor = Color.Silver;
+            createTournamentButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(102, 102, 102);
+            createTournamentButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(242, 242, 242);
+            createTournamentButton.FlatStyle = FlatStyle.Flat;
+            createTournamentButton.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            createTournamentButton.ForeColor = SystemColors.MenuHighlight;
+            createTournamentButton.Location = new Point(190, 347);
+            createTournamentButton.Name = "createTournamentButton";
+            createTournamentButton.Size = new Size(237, 88);
+            createTournamentButton.TabIndex = 34;
+            createTournamentButton.Text = "Create Tournament";
+            createTournamentButton.UseVisualStyleBackColor = true;
+            // 
+            // TournamentDashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(587, 485);
+            Controls.Add(createTournamentButton);
+            Controls.Add(loadTournamentButton);
+            Controls.Add(loadExistingTournamentDropDown);
+            Controls.Add(loadExistingTournamentLabel);
+            Controls.Add(headerLabel);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "TournamentDashboardForm";
+            Text = "Tournament Dashboard";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label headerLabel;
+        private ComboBox loadExistingTournamentDropDown;
+        private Label loadExistingTournamentLabel;
+        private Button loadTournamentButton;
+        private Button createTournamentButton;
+    }
+}
