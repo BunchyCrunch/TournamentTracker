1c3837df41f87c6597faa686d8c8dac041f70df4
bunchycrunch <sparksj1127@gmail.com>
2023-10-10 15:42:19 -0600

    created tournament viewer form

--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -8,9 +8,25 @@ namespace TrackerLibrary
 {
     public class PersonModel
     {
+
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
         public int FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person. 
+        /// </summary>
         public int LastName { get; set; }
+
+        /// <summary>
+        /// The primary email address of the person. 
+        /// </summary>
         public int EmailAddress { get; set; }
+
+        /// <summary>
+        /// The primary cell phone number of the person. 
+        /// </summary>
         public int CellphoneNumber { get; set; }
     }
 }
